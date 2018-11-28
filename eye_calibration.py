b16de8f68358a5cb630b7af9f218f30ba0198147
Florence Chan <florenceut.chan@mail.utoronto.ca>
2018-11-28 01:21:34 -0500

    update eye calibration

--- eye_calibration.py
+++ eye_calibration.py
@@ -1,8 +1,22 @@
-import cv2
-import FaceMarkerDetection
+import numpy as np
+import FaceMarkerDetection as fmd
+import glob
+import os
 
-def get_eye_pose():
-    return NotImplemented
+def get_eye_pose(head_pose):
+    eye_to_head = np.linalg.inv(head_pose)
+    return eye_to_head
 
+
+def get_eye_model():
+    
 
 if __name__ == '__main__':
+    FACES_FOLDER_PATH = "Images/Eye_Calibration"
+    # Iterate through folder:
+    for f in glob.glob(os.path.join(FACES_FOLDER_PATH, "*.jpg")):
+        # For the set of calibration images in this folder the eye to camera matrix is Identity
+        print("Processing file: {}".format(f))
+        shape = fmd.face_marker_detection()
+        head_pose = fmd.hp(shape, f)
+        get_eye_pose(head_pose)
