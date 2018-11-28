b0308063a4281dcc924a55fc07fd21457e34ca69
Florence Chan <florenceut.chan@mail.utoronto.ca>
2018-11-28 01:26:02 -0500

    compute eye pose

--- eye_calibration.py
+++ eye_calibration.py
@@ -8,15 +8,26 @@ def get_eye_pose(head_pose):
     return eye_to_head
 
 
-def get_eye_model():
+def get_eye_model_3d(x, y):
+    """
+    Using the calibration model return theta and phi as a function of x and y
+    :param x: centre x coordinate of the eye ball in 2d
+    :param y: centre y coordinate of the eye ball in 2d
+    :return:
+    """
+    return NotImplemented
     
 
 if __name__ == '__main__':
     FACES_FOLDER_PATH = "Images/Eye_Calibration"
+    predictor_path = "Predictor/shape_predictor_68_face_landmarks.dat"
+
     # Iterate through folder:
     for f in glob.glob(os.path.join(FACES_FOLDER_PATH, "*.jpg")):
         # For the set of calibration images in this folder the eye to camera matrix is Identity
         print("Processing file: {}".format(f))
-        shape = fmd.face_marker_detection()
-        head_pose = fmd.hp(shape, f)
-        get_eye_pose(head_pose)
+        shape = fmd.face_marker_detection(f, predictor_path)
+        head_pose = fmd.head_pose_estimator(shape, f)
+        eye_pose = get_eye_pose(head_pose)
+        print (eye_pose)
+        # TODO overlay normal onto image at eye centre.
